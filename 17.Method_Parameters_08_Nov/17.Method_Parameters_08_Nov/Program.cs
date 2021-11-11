8ca1fc6b3c7c0507a06f87c97d2ea7b7750deffe
Manthan Dhole <85555944+ManthanDhole@users.noreply.github.com>
2021-11-11 15:48:43 +0530

    Add files via upload

---
+++ 17.Method_Parameters_08_Nov/17.Method_Parameters_08_Nov/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+//4 types of parameters
+
+//1. Value Parameters
+//2. Reference Parameters
+//3. Output Parameters
+//4. Parameter Arrays
+
+namespace _17.Method_Parameters_08_Nov
+{
+    class Program
+    {
+        //Value Parameters and Reference Parameters
+
+        //static void Main()
+        //{
+        //    int i = 10;
+
+        //   // ChangeNumber(i);   Pass by Value
+        //    ChangeNumber(ref i);    //Pass by reference
+
+        //    Console.WriteLine(i);
+        //}
+        ////When you use 'ref' keyword before passing the parameter, it acts as a Pointer to the Location of the i variable
+        //public static void ChangeNumber(ref int j)
+        //{
+        //    j = 20;
+        //}
+
+        //Out Parameters (Output parameters)
+
+        //static void Main()
+        //{
+        //    int Sum;
+        //    int Multiply;
+
+        //    //Use out to return the value of the Variable from function to another variable in the Main method
+        //    //remember that the method does not returns any specific value, hence it is declared as void 
+        //    //And if you want to store the returned value in a Vairable then write the name of the variable after the Out Keyword
+        //    Calculate(10, 20, out Sum, out Multiply);
+        //    Console.WriteLine($"Addition = {Sum} and Multiplication = {Multiply}");
+        //}
+
+        ////Use out keyword to return multiple values 
+        ////Use void while declaring the Function
+        //public static void Calculate(int FirstNum, int SecondNum, out int Sum, out int Product)
+        //{
+        //    Product = FirstNum * SecondNum;
+        //    Sum = FirstNum + SecondNum;
+        //}
+
+        //Parameter Array
+        static void Main()
+        {
+            int[] Numbers = new int[5];
+
+            Numbers[0] = 00;
+            Numbers[1] = 10;
+            Numbers[2] = 20;
+            Numbers[3] = 30;
+            Numbers[4] = 40;
+
+            //ParamsMethod(); //You can choose not to pass any parameters here
+            //ParamsMethod(Numbers); //You have passed the Numbers Array
+
+            ParamsMethod(1, 2, 3);
+        }
+
+        public static void ParamsMethod(params int[] Array)
+        {
+            Console.WriteLine($"The number of elements in the Array is {Array.Length}");
+
+            foreach (int i in Array)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
---
+++ 19.Classes_10_Nov/19.Classes_10_Nov/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+/* New vocabulary used in C#
+1. Data Members - Fields
+2. Function - Method / Behaviour  
+3. This keyword - Refers to the fields of the same class 
+4. Constructor - Used to initialize the fields in a class
+5. Default Constructor - Parameterless constructor, gets invoked when no arguements are passed while creating a new instance/object of the class
+6. Destructor - Declared using Tilde Symbol (~) to destroy the objects created at the runtime, after the completion of the program execution 
+7. Overloading Constructors - Having multiple constructors but different Number and Types of Parameters*/
+
+
+
+namespace _19.Classes_10_Nov
+{
+    class Customer
+    {
+        //Class Fields
+        string _firstName;
+        string _lastName;
+
+        //Default Constructor - Used when no arguements are passed to the called constructor
+        public Customer() : this("No First Name Provided", "No Last Name Provided")
+        {
+
+        }
+
+        //Constructors are used to initialize the class fields and it does not return any value, 
+        //A constructor will have the same name as a class hence you can identify easily that a constructor is being used to build up the data members/fields present in the Class
+        public Customer(string FirstName, string LastName)
+        {
+            // 'This' keyword refers to an instance/object of the class
+            this._firstName = FirstName;
+            _lastName = LastName;
+        }
+
+        //Behaviour
+        public void PrintFullName()
+        {
+            Console.WriteLine($"Your Full Name is {this._firstName} {this._lastName}");
+        }
+
+        //Destructor
+        ~Customer()
+        {
+            //Clean up code
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constructors are called automatically when you create an instance of the class and they are used to initialize the Fields in the class
+            Customer C1 = new Customer("Manthan", "Dhole");
+
+            //Use the object to call the method in the class
+            C1.PrintFullName();
+
+            //Calling the Constructor but not providing any Arguments
+            Customer C2 = new Customer();
+            C2.PrintFullName();
+
+            
+
+        }
+    }
+}
