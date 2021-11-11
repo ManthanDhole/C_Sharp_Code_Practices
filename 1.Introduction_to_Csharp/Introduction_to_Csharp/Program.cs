82d7cd5479f70e30f12ee171f7bb4ff363594f07
Manthan Dhole <85555944+ManthanDhole@users.noreply.github.com>
2021-11-11 12:55:38 +0530

    Add files via upload

---
+++ 1.Introduction_to_Csharp/Introduction_to_Csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;    
+class Program
+    {
+        static void Main1()
+        {
+            System.Console.WriteLine("Welcome to C# Training 1");
+        }
+        static void Main()
+        {
+        System.console.writeline("welcome to c# training");
+        Main1();
+        }
+}
+
